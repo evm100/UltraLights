0f3a7f595c62d294a75ff567b89743a0152b9eeb
Edgar V <54725200+evm100@users.noreply.github.com>
2025-09-14 12:39:27 -0700

    Enable OTA unconditionally

--- UltraNodeV5/components/ul_ota/include/ul_ota.h
+++ UltraNodeV5/components/ul_ota/include/ul_ota.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void ul_ota_start(void);
-void ul_ota_stop(void);
 // Triggered via MQTT: ul/<node_id>/cmd/ota/check
 void ul_ota_check_now(bool force);
 
