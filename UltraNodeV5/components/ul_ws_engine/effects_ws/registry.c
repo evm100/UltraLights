883ae6bbae9e429eb71580b68445cd9876930276
Edgar V <54725200+evm100@users.noreply.github.com>
2025-09-23 20:46:19 -0700

    Remove legacy WS effects

--- UltraNodeV5/components/ul_ws_engine/effects_ws/registry.c
+++ UltraNodeV5/components/ul_ws_engine/effects_ws/registry.c
@@ -6,14 +6,9 @@
 
 void solid_init(void);        void solid_render(uint8_t*,int,int);        void solid_apply_params(int,const cJSON*);
 void color_swell_init(void);  void color_swell_render(uint8_t*,int,int);  void color_swell_apply_params(int,const cJSON*);
-void breathe_init(void);      void breathe_render(uint8_t*,int,int);
 void rainbow_init(void);      void rainbow_render(uint8_t*,int,int);      void rainbow_apply_params(int,const cJSON*);
 void modern_rainbow_init(void); void modern_rainbow_render(uint8_t*,int,int);
-void twinkle_init(void);      void twinkle_render(uint8_t*,int,int);
-void theater_chase_init(void);void theater_chase_render(uint8_t*,int,int);
-void wipe_init(void);         void wipe_render(uint8_t*,int,int);
 void noise_init(void);        void noise_render(uint8_t*,int,int);
-void gradient_scroll_init(void);void gradient_scroll_render(uint8_t*,int,int);
 void triple_wave_init(void);  void triple_wave_render(uint8_t*,int,int);   void triple_wave_apply_params(int,const cJSON*);
 void flash_init(void);        void flash_render(uint8_t*,int,int);        void flash_apply_params(int,const cJSON*);
 void spacewaves_init(void);   void spacewaves_render(uint8_t*,int,int);   void spacewaves_apply_params(int,const cJSON*);
@@ -25,13 +20,8 @@ void black_ice_init(void);    void black_ice_render(uint8_t*,int,int);    void b
 static const ws_effect_t effects[] = {
     {"solid", WS_EFFECT_TIER_STANDARD, solid_init, solid_render, solid_apply_params},
     {"color_swell", WS_EFFECT_TIER_STANDARD, color_swell_init, color_swell_render, color_swell_apply_params},
-    {"breathe", WS_EFFECT_TIER_STANDARD, breathe_init, breathe_render, NULL},
     {"rainbow", WS_EFFECT_TIER_STANDARD, rainbow_init, rainbow_render, rainbow_apply_params},
     {"modern_rainbow", WS_EFFECT_TIER_STANDARD, modern_rainbow_init, modern_rainbow_render, NULL},
-    {"twinkle", WS_EFFECT_TIER_STANDARD, twinkle_init, twinkle_render, NULL},
-    {"theater_chase", WS_EFFECT_TIER_STANDARD, theater_chase_init, theater_chase_render, NULL},
-    {"wipe", WS_EFFECT_TIER_STANDARD, wipe_init, wipe_render, NULL},
-    {"gradient_scroll", WS_EFFECT_TIER_STANDARD, gradient_scroll_init, gradient_scroll_render, NULL},
     {"triple_wave", WS_EFFECT_TIER_STANDARD, triple_wave_init, triple_wave_render, triple_wave_apply_params},
     {"flash", WS_EFFECT_TIER_STANDARD, flash_init, flash_render, flash_apply_params},
     {"spacewaves", WS_EFFECT_TIER_STANDARD, spacewaves_init, spacewaves_render, spacewaves_apply_params},
